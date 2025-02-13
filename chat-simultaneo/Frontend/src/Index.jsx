58469800a70ce187d066b3b9fb36e51f86dfbd61
Bruno de Fraga <brunofragaa97@gmail.com>
2025-02-13 10:32:06 -0300

    configurado ambiente de desenvolvimento com react+vite

---
+++ chat-simultaneo/Frontend/src/Index.jsx
@@ -0,0 +1,17 @@
+import '../CSS/desktop/index.css'
+import Login from './login';
+
+function Index() {
+
+
+
+
+return <>
+    <div className="background-container">
+        <Login />
+    </div>
+</>
+
+}
+
+export default Index; 
---
+++ chat-simultaneo/Frontend/src/Login.jsx
@@ -0,0 +1,65 @@
+import '../CSS/desktop/login.css'
+import React, { useState } from "react";
+
+    const Login = () => {
+        const [formData, setFormData] = useState({
+          email: "",
+          senha: "",
+        });
+      
+        const [errors, setErrors] = useState({}); // Inicializa como objeto vazio
+        const [inputError, setInputError] = useState({});
+      
+        const handleChange = (e) => {
+          const { name, value } = e.target;
+          setFormData((prevData) => ({ ...prevData, [name]: value }));
+          setInputError((prevErrors) => ({ ...prevErrors, [name]: "" })); // Limpa erro ao digitar
+        };
+      
+        const validateForm = () => {
+          const newErrors = {};
+          Object.keys(formData).forEach((key) => {
+            if (!formData[key]) {
+              newErrors[key] = "Este campo é obrigatório.";
+            }
+          });
+          setInputError(newErrors);
+          return Object.keys(newErrors).length === 0;
+        };
+      
+        
+      
+        return (
+          <div className="form-container-login">
+            <h1>ENTRE COM O EMAIL</h1>
+            <form  className="client-form-login">
+              {[
+                { label: "E-mail", name: "email", type: "email" },
+                { label: "Senha", name: "senha", type: "password" },
+              ].map((field) => (
+                <div key={field.name} className="form-group">
+                  <label htmlFor={field.name}>{field.label}</label>
+                  <input
+                    type={field.type}
+                    id={field.name}
+                    name={field.name}
+                    value={formData[field.name]}
+                    onChange={handleChange}
+                    className={inputError[field.name] ? "input-error" : ""}
+                  />
+                  {inputError[field.name] && <small>{inputError[field.name]}</small>}
+                </div>
+              ))}
+      
+              {/* Exibe a mensagem de erro do login abaixo do formulário */}
+              {errors.login && <div className="error-message">{errors.login}</div>}
+      
+              <button type="submit" className="submit-button-login">
+                ENTRAR
+              </button>
+            </form>
+          </div>
+        );
+      };
+
+export default Login;
