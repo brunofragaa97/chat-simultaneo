f92e9d73d7d62df686759f0163ad55b251447180
Bruno de Fraga <brunofragaa97@gmail.com>
2025-02-17 21:14:18 -0300

    adicionado comentarios na classe ExecoesGlobais

--- chat-simultaneo/Backend/Api-chat-simultaneo/src/main/java/org/chat_simultaneo/Exeptions/ExecoesGlobais.java
+++ chat-simultaneo/Backend/Api-chat-simultaneo/src/main/java/org/chat_simultaneo/Exeptions/ExecoesGlobais.java
@@ -10,20 +10,30 @@ import org.chat_simultaneo.DTO.ErrorResponse;
 
 import java.time.LocalDateTime;
 
+// Define esta classe como um manipulador global de exceções no Spring Boot
 @ControllerAdvice
 public class ExecoesGlobais {
 
+    // Anota um método que lida com exceções do tipo DataIntegrityViolationException
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
-        // Se for uma violação de restrição do banco, trata como e-mail duplicado
+
+        // Verifica se a causa raiz do erro é uma violação de restrição do banco de dados
         if (ex.getCause() instanceof ConstraintViolationException) {
+            // Define uma mensagem específica para erro de e-mail duplicado
             String message = "E-mail já cadastrado.";
+
+            // Cria um objeto ErrorResponse contendo um código de erro, mensagem e timestamp
             ErrorResponse errorResponse = new ErrorResponse("ERR_DUPLICATE_EMAIL", message, LocalDateTime.now());
+
+            // Retorna a resposta com status HTTP 400 (Bad Request) e o corpo da mensagem de erro
             return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
         }
 
-        // Se for outra violação, retorne um erro genérico
+        // Se a exceção não for por e-mail duplicado, cria uma resposta de erro genérico do banco de dados
         ErrorResponse errorResponse = new ErrorResponse("ERR_DATABASE", "Erro de integridade do banco de dados.", LocalDateTime.now());
+
+        // Retorna a resposta com status HTTP 500 (Internal Server Error)
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
